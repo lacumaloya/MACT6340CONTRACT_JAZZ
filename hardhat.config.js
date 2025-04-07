e0cf4551b7a7cbe0da89e742cb30965e1bc42748
Jasmine Aguilar <jasmineaguilar@mail.smu.edu>
2025-04-06 23:52:54 -0500

    waiting for faucets to replenish so i can resolve missing usd data in gas report

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,10 +6,10 @@ const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 
 // change these for different networks
-const ALCHEMY_URL = `https://polygon-amoy.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
-//const ALCHEMY_URL = `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
-//const ALCHEMY_URL = `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
-//const ALCHEMY_URL = `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
+const POLYGON_AMOY_URL = `https://polygon-amoy.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
+const POLYGON_MAINNET_URL = `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
+const SEPOLIA_URL = `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
+const ETHEREUM_URL = `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
 
 const STUNT_WALLET_PRIVATE_KEY = process.env.STUNT_WALLET_PRIVATE_KEY;
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
@@ -24,50 +24,50 @@ module.exports = {
     },
   },
   gasReporter: {
-    enabled: false,
+    enabled: true,
     currency: "USD",
     coinmarketcap: COINMARKETCAP_API_KEY,
-    token: "POL",
+    token: "MATIC", // Still "MATIC" for now
     outputFile: "gas-report.txt",
     noColors: true,
-  },
-  defaultNetwork: "hardhat",  // "hardhat" for testing, change this for different networks
+  },  
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 31337,
       gas: 30000000,
-      blockGasLimit: 30000000, // add this line
+      blockGasLimit: 30000000,
     },
     polygonAmoy: {
-      url: ALCHEMY_URL,
+      url: POLYGON_AMOY_URL,
       accounts: [STUNT_WALLET_PRIVATE_KEY],
       gasPrice: 35000000000,
       chainId: 80002,
     },
     polygon: {
-      url: ALCHEMY_URL,
+      url: POLYGON_MAINNET_URL,
       accounts: [STUNT_WALLET_PRIVATE_KEY],
       gasPrice: 35000000000,
       chainId: 137,
     },
     sepolia: {
-      url: ALCHEMY_URL,
+      url: SEPOLIA_URL,
       accounts: [STUNT_WALLET_PRIVATE_KEY],
       gasPrice: 35000000000,
       chainId: 11155111,
     },
     ethereum: {
-      url: ALCHEMY_URL,
+      url: ETHEREUM_URL,
       accounts: [STUNT_WALLET_PRIVATE_KEY],
       gasPrice: 35000000000,
       chainId: 1,
     },
   },
   solidity: {
-    version: "0.8.22", // use an exact version here and in contract to avoid verification problems
+    version: "0.8.22",
     settings: {
       optimizer: {
-        enabled: false, // may cause verification problems if true
+        enabled: false,
       },
     },
   },
